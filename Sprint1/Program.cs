46991efba15a040aced641b8dae80812c72611ad
João Victor da Encarnação Cunha <158203459+xeilongue@users.noreply.github.com>
2026-02-05 11:41:15 -0300

    Add files via upload

---
+++ Sprint1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace SistemaBancario
+{
+
+    abstract class ContaBancaria
+    {
+        protected int numero;
+        protected string titular;
+        protected double saldo;
+
+        public int Numero
+        {
+            get { return numero; }
+            set { numero = value; }
+        }
+
+        public string Titular
+        {
+            get { return titular; }
+            set { titular = value; }
+        }
+
+        public double Saldo
+        {
+            get { return saldo; }
+            set { saldo = value; }
+        }
+
+        public ContaBancaria(int numero, string titular, double saldo)
+        {
+            this.numero = numero;
+            this.titular = titular;
+            this.saldo = saldo;
+        }
+
+    }
+
+    class ContaCorrente : ContaBancaria
+    {
+        
+        public ContaCorrente(int numero, string titular, double saldo) : base(numero, titular, saldo)
+        {
+
+        }
+
+
+    }
+
+    class ContaPoupanca : ContaBancaria
+    {
+
+        public ContaPoupanca(int numero, string titular, double saldo) : base(numero, titular, saldo)
+        {
+
+        }
+    }
+
+    class ContaEmpresarial : ContaBancaria
+    {
+
+        public ContaEmpresarial(int numero, string titular, double saldo) : base(numero, titular, saldo)
+        {
+
+        }
+
+    }
+
+
+
+    class Program
+    {
+
+        static void Main()
+        {
+        
+        }
+    }
+}
